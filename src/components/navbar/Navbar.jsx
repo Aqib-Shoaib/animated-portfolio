b39a34c5bacc17d08347ba51c2b87ac5eb059b13
Aqib-Shoaib <bluepitviper1233@gmail.com>
2024-11-04 17:02:07 +0500

    nav and sidebar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,21 @@
+import Sidebar from "../sidebar/sidebar";
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 function Navbar() {
   return (
     <div className="navbar">
       {/* sidebar */}
+      <Sidebar />
+
       <div className="wrapper">
-        <span>Aqib Shoaib</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.2 }}
+        >
+          Aqib Shoaib
+        </motion.span>
         <div className="social">
           <a href="">
             <img src="/facebook.png" alt="" />
